cc2bac457f465442dd10c25e1c4998cf6469e75b
Rose Nasrawi <rosenasrawi@Roses-MacBook-Pro.local>
2021-12-10 22:57:50 +0100

    part two complete

--- 10/10.py
+++ 10/10.py
@@ -2,7 +2,7 @@ with open("/Users/rosenasrawi/Documents/VU PhD/Side projects/CodeAdvent2021/10/i
     syntax = syntaxFile.readlines()
     syntax = [i.rstrip("\n") for i in syntax]
 
-# syntax = ['[({(<(())[]>[[{[]{<()<>>','[(()[<>])]({[<{<<[]>>(','{([(<{}[<>[]}>{[]{[(<()>','(((({<>}<{<{<>}{[]{[]{}','[[<[([]))<([[{}[[()]]]','[{[{({}]{}}([{[{{{}}([]','{<[[]]>}<{[{[{[]{()[[[]','[<(<(<(<{}))><([]([]()','<{([([[(<>()){}]>(<<{{','<{([{{}}[<[[[<>{}]]]>[]]']
+from statistics import median
 
 # Part 1 - Find first incorrect item in syntax
 
@@ -24,8 +24,36 @@ def firstWrongSyntax(line):
         score = points[close.index(firstFound)]
     return score
 
-totalScore = 0
+totalScore = 0; lineScores = []
 for line in syntax:
-    totalScore += firstWrongSyntax(line)
+    score = firstWrongSyntax(line); lineScores.append(score)
+    totalScore += score
 
 print(totalScore)
+
+# Part 2 - complete the incomplete syntax
+
+incomplete = [syntax[i] for i in range(len(lineScores)) if lineScores[i] == 0]
+
+def fixIncompleteSyntax(line):
+    open = '([{<'; close = ')]}>'; points = [1,2,3,4]
+    started = []
+
+    for i in line:
+        if i in open:
+            started.append(i)
+        elif i in close:
+            if close.index(i) == open.index(started[-1]):
+                started.pop()               
+
+    score = 0; started.reverse()
+    for i in started:
+        p = points[open.index(i)]
+        score = score*5+p
+    return score      
+
+lineScores = []
+for line in incomplete:
+    lineScores.append(fixIncompleteSyntax(line))
+
+print(median(lineScores))
