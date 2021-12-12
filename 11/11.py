bec1a6847b53d4e2fd9a992cc7ce02a87028c0ed
Rose Nasrawi <rosenasrawi@Roses-MacBook-Pro.local>
2021-12-12 10:38:42 +0100

    part two complete

--- 11/11.py
+++ 11/11.py
@@ -1,11 +1,14 @@
-with open("/Users/rosenasrawi/Documents/VU PhD/Side projects/CodeAdvent2021/11/input11.txt", "r") as octopiFile:
-    octopi = octopiFile.readlines()
-    octopi = [i.rstrip("\n") for i in octopi]
-    for i in range(len(octopi)): octopi[i] = list(map(int,octopi[i]))
+# Open data file
+def getInput():
+    with open("/Users/rosenasrawi/Documents/VU PhD/Side projects/CodeAdvent2021/11/input11.txt", "r") as octopiFile:
+        octopi = octopiFile.readlines()
+        octopi = [i.rstrip("\n") for i in octopi]
+        for i in range(len(octopi)): octopi[i] = list(map(int,octopi[i]))
+    return octopi
 
 # octopi = [[5,4,8,3,1,4,3,2,2,3],[2,7,4,5,8,5,4,7,1,1],[5,2,6,4,5,5,6,1,7,3],[6,1,4,1,3,3,6,1,4,6],[6,3,5,7,3,8,5,4,7,8],[4,1,6,7,5,2,4,6,4,5],[2,1,7,6,8,4,1,7,2,1],[6,8,8,2,8,8,1,1,3,4],[4,8,4,6,8,4,8,5,5,4],[5,2,8,3,7,5,1,5,2,6]]
-# octopi = [[1,1,1,1,1],[1,9,9,9,1],[1,9,1,9,1],[1,9,9,9,1],[1,1,1,1,1]]
 
+# Find adjacent points in matrix (incl diagonal)
 def adjacent(matrix, x, y):
     if x == 0:
         if y == 0: adjIndex = [[x,y+1], [x+1,y], [x+1, y+1]]
@@ -22,6 +25,7 @@ def adjacent(matrix, x, y):
 
     return adjIndex
 
+# Simulate flashing octopi
 def flashingOctopi(octopi):
     flashed = [] # flashed = empty
     queue = []
@@ -49,12 +53,20 @@ def flashingOctopi(octopi):
     return len(flashed)
 
 # Part 1 - count total amount of flashes after 100 steps
+octopi = getInput(); totalFlashed = 0
 
-totalFlashed = 0
 for i in range(100):
     totalFlashed += flashingOctopi(octopi)
     
 print(totalFlashed)
 
+# Part 2 - find first time all flash simultaneously
+octopi = getInput(); simultaneous = False; 
+numOctipi = len(octopi)*len(octopi[0]); steps = 0
 
+while not simultaneous:
+    flashed = flashingOctopi(octopi); steps+=1
+    if flashed == numOctipi:
+        simultaneous = True 
 
+print(steps)
