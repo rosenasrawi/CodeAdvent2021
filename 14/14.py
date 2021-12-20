8b371632ce9945ff23de3534d951b75a1a888ccf
Rose Nasrawi <rosenasrawi@Roses-MacBook-Pro.local>
2021-12-20 13:56:47 +0100

    Part two with example, counting letters

--- 14/14.py
+++ 14/14.py
@@ -1,42 +1,58 @@
-# Open data file
-with open("/Users/rosenasrawi/Documents/VU PhD/Side projects/CodeAdvent2021/14/input14.txt", "r") as polyFile:
-    poly = polyFile.readlines()
+def preprocessData(datafile):
+    with open("/Users/rosenasrawi/Documents/VU PhD/Side projects/CodeAdvent2021/14" + datafile, "r") as polyFile:
+        poly = polyFile.readlines()
 
-    polymer = list(poly.pop(0).rstrip("\n")); poly.pop(0)
-
+    polymer = poly.pop(0).rstrip("\n"); poly.pop(0)
     poly = [i.rstrip("\n").split(' -> ') for i in poly]
 
-    connections = []; insertions = []
+    connections = []; counts = []; insertions = {}
     for i in poly:
-        connections.append(list(i[0]))
-        insertions.append(i[1])
-
-
-# Example data
-# polymer = ['N', 'N', 'C', 'B']
-# connections = [['C','H'],['H','H'],['C','B'],['N','H'],['H','B'],['H','C'],['H','N'],['N','N'],['B','H'],['N','C'],['N','B'],['B','N'],['B','B'],['B','C'],['C','C'],['C','N']]
-# insertions = ['B','N','H','C','C','B','C','C','H','B','B','B','N','B','N','C']
-
-
-# Function to insert to polymer
-def insertPolymer(polymer, connections, insertions):
-    insIndex = []; polyIndex = []
+        connections.append(i[0])                                                # All types of connections
+        counts.append(0)                                                        # count of connections
+        insertions[i[0]] = [''.join(i[0][0] + i[1]), ''.join(i[1] + i[0][1])]   # What connection becomes after insertion
 
     for i in range(len(polymer)-1):
-        insIndex.append(connections.index(polymer[i:i+2]))
-        polyIndex.append(i*2+1)
+        counts[connections.index(polymer[i:i+2])] += 1                          # Change polymer to count of connections
 
-    for i in range(len(polyIndex)):
-        ins = insertions[insIndex[i]]
-        polymer.insert(polyIndex[i],ins)
+    return counts, connections, insertions
 
-    return polymer
+def insertPolymer(counts, connections, insertions):
 
-# Part 1 - count least and most occuring in polymer after 10 steps of insertion
+    num2add = []; index2rem = []; index2add = []
+    for i in range(len(counts)):
+        if counts[i] > 0:
+            num2add.append(counts[i])
+            index2rem.append(i)
+
+            con = connections[i]; ins = insertions[con]
+            index2add.append([connections.index(ins[0]), connections.index(ins[1])])
+
+    for i in range(len(num2add)):
+        counts[index2rem[i]] -= num2add[i]
+
+    for i in range(len(num2add)):
+        j = index2add[i]
+        counts[j[0]] += num2add[i]
+        counts[j[1]] += num2add[i]
+
+    return counts
+
+counts, connections, insertions = preprocessData(datafile = "/example14.txt")
+
 for i in range(10):
-    polymer = insertPolymer(polymer, connections, insertions)
+    counts = insertPolymer(counts, connections, insertions)
 
-mostComm = polymer.count(max(set(polymer), key = polymer.count))
-leastComm = polymer.count(min(set(polymer), key = polymer.count))
+letters = list(set(''.join(connections)))
+letterPosCount = [[0,0] for i in letters]
 
-print(mostComm-leastComm)
+for i in range(len(connections)):
+    if counts[i] > 0:
+        con = connections[i]
+        letterPosCount[letters.index(con[0])][0] += counts[i]
+        letterPosCount[letters.index(con[1])][1] += counts[i]
+
+for i in range(len(letterPosCount)):
+    letterPosCount[i] = max(letterPosCount[i])
+
+print(letters)
+print(letterPosCount)
