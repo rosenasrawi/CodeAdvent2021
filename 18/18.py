9a69eb48eb48afca9ef1c210b4abfa72bd4f642d
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-11-23 09:54:09 +0100

    Nearly there part 1

--- 18/18.py
+++ 18/18.py
@@ -1,38 +1,89 @@
+import math
 
-# def add(snailnum1, snailnum2):
-    # snailnum = [snailnum1, snailnum2]
-    # return snailnum
+# Add two snailnumbers
+def add(snailnums):
+    snailnum = '['+ snailnums[0] + ',' + snailnums[1] + ']'   
+    return snailnum
 
-# def reduce(snailnum): 
-    # while True:
+# Snailnumber string to list
+def string2list(snailnum):
+    snaillist = []
+    for i in list(range(len(snailnum))):
+        if len(snaillist) > 2 and snailnum[i-1:i+1].isnumeric():
+            snaillist[-1] += snailnum[i]
+        else: snaillist.append(snailnum[i])
+
+    return snaillist
+
+# Reduce snailnum
+def reduce(snailnum):
+    snailnum = string2list(snailnum)
+    boom = False; crack = False
+    nest = 0; i = 0
+
+    while True:
         
-        # restart = False
+        if snailnum[i] == '[': nest += 1
+        elif snailnum[i] == ']': nest -= 1
+        
+        if nest == 5: # Explode
+            
+            pair = [i+1,i+3]
+            l = pair[0]-1; r = pair[1]+1
 
-        # If pair nested inside four pares:
-            # snailnum = explode(leftmost pair):
-            # restart = True  
+            while True:
+                if l == 0: break
+                if snailnum[l].isnumeric():
+                    snailnum[l] = str(int(snailnum[pair[0]]) + int(snailnum[l])); break
+                l-=1
 
-        # elif any number >= 10 and not restart:
-            # snailnum = split(leftmost such number):
+            while True:
+                if r == len(snailnum)-1: break
+                if snailnum[r].isnumeric():
+                    snailnum[r] = str(int(snailnum[pair[1]]) + int(snailnum[r])); break
+                r+=1
 
-        # else: break
+            snailnum[pair[0]-1:pair[1]+2] = '0'
 
-        # only the first in the list that applies, then back to while loop
+            boom = True
+            break
+
+        if snailnum[i].isnumeric(): # Split
+            if int(snailnum[i]) >= 10: 
+                half = int(snailnum[i])/2
+                snailnum[i] = '['+ str(math.floor(half)) + ',' + str(math.ceil(half)) + ']'
+
+                crack = True
+                break 
+
+        i+=1
+
+        if i == len(snailnum)-1: break # End of snailnum
+
+    return ''.join(snailnum), boom, crack
+
+# Reduce snailnum with explosions & splits
+def snailmath(snailnums):
+
+    while True:
+        if len(snailnums) == 1: break
+        snailnum = add(snailnums)
+        print(snailnum)
     
-    # return snailnum
+        while True:
+            snailnum, boom, crack = reduce(snailnum)
 
-# def explode(snailnum):
-    # pair[0] + first regular number to the left (if any)
-    # pair[1] + first regular number to the right (if any)
-    # pair becomes a zero
+            if not boom and not crack: break
 
-    #[[6,[5,[4,[3,2]]]],1] -> [3,2] = nested pair
-    #[[6,[5,[4+3,0]]]],1+2] -> 3 to the 3, 2 to the 1, pair = 0
-    # return snailnum
+        print(snailnum)
+        snailnums[0] = snailnum; snailnums.pop(1)
 
-# def split(snailnum)
-    # num = [num/2 (rounded down), num/2 (rounded up)]
-    # return snailnum
+    return snailnum
+
+
+snailnums = ['[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]' , '[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]']
+
+snailmath(snailnums)
 
 # def sum(snailnums):
     # while pair:
